86db606e1480340d85c377876ed7d7d92f316ef1
arir2001 R <amtrooney@gmail.com>
2024-04-23 17:05:16 +0000

    while loops added to name and instrruction corrections to ensure valid answers inputed.

--- run.py
+++ run.py
@@ -1,16 +1,18 @@
 # Your code goes here.
 import time
 
-def machineprint(text, delay = 0.1):
+#to slow print text
+def machineprint(text, delay = 0.01):
     for letter in text:
         print(letter, end="", flush=True)
         time.sleep(delay)
     print()
 
+#the instructions to the game
 instruct = " these are the instructions"
 
 
-
+#welcoming the game 
 def welcome_message():
     machineprint("""
         Hello there! 
@@ -26,19 +28,52 @@ def welcome_message():
         Do you wish to see the instructions?
         Please answer 'Y' for yes or 'N' for no.""")
 
-    instructions = input('View instructions?:')
+    x = 0
 
-    if instructions == 'Y': 
-        print(instruct)
-    elif instructions == 'N':
-        playgame()
+    #to see instructions
+    while x != 1:
+        INSTRUCTIONS = input('View instructions?:')
+        if INSTRUCTIONS == 'Y': 
+            print(instruct)
+            name()
+            x == 1
+            print(x)
+        elif INSTRUCTIONS == 'N':
+            name()
+            x == 1
+            print(x)
+        else: 
+            machineprint("That is an invalid input. Please try again.")
+            print(x)
+
+welcome_message()         
+
+def name():
+    machineprint("What is your name?")
+    NAME = input('Your name: ')
+    CORRECT = input("Is this correct? Enter 'Y' or 'N':" )
+
+    x=0
+
+    while x != 1:
+    if CORRECT == 'Y': 
+        print("Yay! Time to play," , NAME, "!")
+        x == 1
+    elif CORRECT == 'N':
+        name()
+        x==1
     else: 
         machineprint("That is an invalid input. Please try again.")
-        instructions
+        
+
+
+ 
+
+     
+
 
 def playgame():
     print('playing game')
 
 
 
-welcome_message()
