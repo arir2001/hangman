33910279b44da7f4508bd260fc7ffd5ddbf43e89
arir2001 R <amtrooney@gmail.com>
2024-04-23 14:31:51 +0000

    machine print function from stack exchange inserted. pprint and time imported.

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 # Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from pprint import pprint
+import time
+
+def machineprint(text, delay = 0.1):
+    for letter in text:
+        print(letter, end="", flush=True)
+        time.sleep(delay)
+    print()
