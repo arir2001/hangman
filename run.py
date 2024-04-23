597485d613b0be793513f8cac51d96751b486d77
arir2001 R <amtrooney@gmail.com>
2024-04-23 14:53:50 +0000

    welcome message with defensive design on whether to view instructions or not created.

--- run.py
+++ run.py
@@ -7,65 +7,38 @@ def machineprint(text, delay = 0.1):
         time.sleep(delay)
     print()
 
+instruct = " these are the instructions"
+
+
 
 def welcome_message():
     machineprint("""
         Hello there! 
-        Welcome to this little game of Hangman!
+        Welcome to this little game of Hangman! 
+            +---+
+            |   |
+            O   |
+           /|\  |
+           / \  |
+                |
+        =========
+
+        Do you wish to see the instructions?
+        Please answer 'Y' for yes or 'N' for no.""")
+
+    instructions = input('View instructions?:')
+
+    if instructions == 'Y': 
+        print(instruct)
+    elif instructions == 'N':
+        playgame()
+    else: 
+        machineprint("That is an invalid input. Please try again.")
+        instructions
+
+def playgame():
+    print('playing game')
 
 
 
-    """)
-
-
-
-HANGMANPICS = ['''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========''']
+welcome_message()
